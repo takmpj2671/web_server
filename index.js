5d1e70a06a7042bc5af45dcb81cb383e2dae83bd
T.Yusuke <taklahashi0711@outlook.jp>
2025-08-31 17:30:06 +0900

    Heroku Deply first  change1

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.get('/api/v1/users', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log('Running server')
 });
